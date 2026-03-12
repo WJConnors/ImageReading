e36daa879cab7e53cdfbe4fce647bfa167a1b9d3
WJConnors <37446973+WJConnors@users.noreply.github.com>
2026-03-12 20:38:32 +0000

    Learned memcmp exists and found a snazzy one liner

--- pngReader.c
+++ pngReader.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define PNG_SIG "\x89PNG\r\n\x1a\n"
 
 int main() {
-    FILE* fptr = fopen("pngtest.png", "rb");
+    FILE* fptr = fopen("pngTest.png", "rb");
 
     if (fptr == NULL) {
         printf("couldn't open image file");
         return 1;
     }
 
-    
     printf("opened file\n");
 
-    int expected[] = {137, 80, 78, 71, 13, 10, 26, 10};
-    for (int i = 0; i < 8; i++) {
-        int byte = fgetc(fptr);
-        if (byte != expected[i]) {
-            printf("Not a PNG");
-            fclose(fptr);
-            return 1;
-        }
-    }
+    char sig[sizeof(PNG_SIG) - 1];
+    bool isPNG = fread(sig, 1, sizeof(sig), fptr) == sizeof(sig) &&
+           memcmp(sig, PNG_SIG, sizeof(sig)) == 0;
+
+    printf("%d", isPNG);
 
     fclose(fptr);
     return 0;
