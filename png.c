9deb578e368277eb6e036a21806f0b397669876b
WJConnors <37446973+WJConnors@users.noreply.github.com>
2026-03-20 11:02:12 +0000

    Generic helper readBytes function

--- png.c
+++ png.c
@@ -6,8 +6,12 @@
 
 #define PNG_SIG "\x89PNG\r\n\x1a\n"
 
+static bool readBytes(FILE* fptr, unsigned char* buffer, size_t readSize) {
+    return fread(buffer, 1, readSize, fptr) == readSize;
+}
+
 bool validatePNG(FILE* fptr) {
     unsigned char sig[sizeof(PNG_SIG) - 1];
-    return fread(sig, 1, sizeof(sig), fptr) == sizeof(sig) &&
-           memcmp(sig, PNG_SIG, sizeof(sig)) == 0;
+    return readBytes(fptr, sig, sizeof(sig)) &&
+            memcmp(sig, PNG_SIG, sizeof(sig)) == 0;
 }
