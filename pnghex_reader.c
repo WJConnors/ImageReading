8c1570585722234bef6fbe057456748a0998bd1d
WJConnors <37446973+WJConnors@users.noreply.github.com>
2026-04-03 15:50:03 +0100

    Initial chunk size and type out

--- pnghex_reader.c
+++ pnghex_reader.c
@@ -3,6 +3,39 @@
 
 #include "png.h"
 
+/*
+8 byte png sig
+Then chunks:
+    4 bytes size
+    4 bytes ID
+    x bytes depending on size
+    4 bytes CRC
+*/
+
+static void readChunk(FILE* fptr, FILE* out) {
+    unsigned int size;
+    readChunkSize(fptr, &size);
+    fprintf(out, "Chunk Size: %i\n", size);
+
+    unsigned char type[4];
+    readBytes(fptr, type, 4);
+    
+    for (int i = 0; i < 4; i++) {
+        fprintf(out, "%02X ", type[i]);
+    }
+    fprintf(out, " | ");
+
+    for (int i = 0; i < 4; i++) {
+        if (isprint(type[i])) {
+            fprintf(out, "%c", type[i]);
+        } else {
+            fprintf(out, ".");
+        }
+    }   
+
+    fprintf(out, "\n");
+}
+
 int main() {
     FILE* fptr = fopen("pngTest.png", "rb");
     FILE* out = fopen("HEX.txt", "w");
@@ -29,6 +62,8 @@ int main() {
     }   
 
     fprintf(out, "\n");
+
+    readChunk(fptr, out);
 
     fclose(fptr);
     fclose(out);
